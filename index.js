e3a469c0aa29c4bbd6af21b5878e9663594802d0
TzipiReiss <tz0583204266@gmail.com>
2023-05-09 17:22:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// import express from 'express'
+// import cors from 'cors'
+// import bodyParser from 'body-parser';
+var express = require('express');
+const app = express();
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
+const port = 3000
+// app.use(cors());
+// app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    sdk.auth('rnd_DSG850Fp9dgj5axph7x72Qi7Jjdm');
+    sdk.getServices({limit: '20'})
+      .then(({ data }) => res.send(data))
+      .catch(err => console.error(err));
+})
+
+app.listen(port, () => {
+    console.log('Example app listening on http://localhost:3000');
+})
+
