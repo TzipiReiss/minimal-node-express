f24bae053ab24e5b029268d606204d31d375c0d2
TziporaReiss <116269752+TzipiReiss@users.noreply.github.com>
2023-05-11 19:56:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-// import express from 'express'
-// import cors from 'cors'
-// import bodyParser from 'body-parser';
 var express = require('express');
 const app = express();
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 const PORT = process.env.PORT || 8081;
-// app.use(cors());
-// app.use(bodyParser.json());
 
-  app.get("/",async (req, res) => {
-    sdk.auth('rnd_DSG850Fp9dgj5axph7x72Qi7Jjdm');
-    sdk.getServices({limit: '20'})
-      .then(({ data }) =>  res.json(data))
-      .catch(err => console.error(err));
- })
+app.get("/",async (req, res) => {
+  sdk.auth('rnd_DSG850Fp9dgj5axph7x72Qi7Jjdm');
+  sdk.getServices({limit: '20'})
+    .then(({ data }) =>  res.json(data))
+    .catch(err => console.error(err));
+})
 
- app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
- })
+app.listen(PORT, () => {
+ console.log(`server started on port ${PORT}`);
+})
